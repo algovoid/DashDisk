e44a7cc2db9ba1d62b18219aaa9f2a65de57ee2d
Algovoid <87210332+algovoid@users.noreply.github.com>
2025-10-30 18:13:32 +0100

    init

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DashDisk.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DashDisk.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
